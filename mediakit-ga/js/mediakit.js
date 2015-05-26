68e243dedc704397feaaf7e92f9720a6fc35ac67
Cristian <calvarado@bow.cl>
2015-05-26 18:20:11 -0300

    update mobile

--- mediakit-ga/js/mediakit.js
+++ mediakit-ga/js/mediakit.js
@@ -1,23 +1,217 @@
 jQuery(document).ready(function($) {
-	$("#fb_likes").empty().html( numberWithCommas( fb_likes ) );
-	$("#twitter_followers").empty().html( numberWithCommas( twitter_followers ) );
-	$("#instagram_followers").empty().html( numberWithCommas( instagram_followers ) );
+	if ( is_single !== undefined && is_single === 1 ) 
+	{
 
-	//GA
-	$("#sessions_count").empty().html( numberWithCommas( sessions_count ) );
-	$("#users_count").empty().html( numberWithCommas( users_count ) );
-	$("#avg_sessions_duration").empty().html( secondsToMinSeconds( avg_sessions_duration ) );
-	$("#pageviews_count").empty().html( numberWithCommas( pageviews_count ) );
-	$("#pageviews_per_session").empty().html( pageviews_per_session );
+		//Facebook Calls
+		var data = {
+			'action': 'fb_calls',
+			'post_id': post_id
+		};
+		$.ajax({
+			method: "POST",
+			url: ajaxurl,
+			data: data,
+			dataType: 'json'
+		})
+			.done(function( response ) {
+				if ( response.error !== undefined && response.error == false ) 
+				{
+					$("#fb_likes").empty().html( numberWithCommas( response.data.likes_fb ) );
+				};
+			});
+
+		//Twitter Calls
+		var data = {
+			'action': 'tw_calls',
+			'post_id': post_id
+		};
+		$.ajax({
+			method: "POST",
+			url: ajaxurl,
+			data: data,
+			dataType: 'json'
+		})
+			.done(function( response ) {
+				if ( response.error !== undefined && response.error == false ) 
+				{
+					$("#twitter_followers").empty().html( numberWithCommas( response.data.twitter_count ) );
+				};
+			});
+
+		//Instagram Calls
+		var data = {
+			'action': 'inst_calls',
+			'post_id': post_id
+		};
+		$.ajax({
+			method: "POST",
+			url: ajaxurl,
+			data: data,
+			dataType: 'json'
+		})
+			.done(function( response ) {
+				if ( response.error !== undefined && response.error == false ) 
+				{
+					$("#instagram_followers").empty().html( numberWithCommas( response.data.instagram_count ) );
+				};
+			});
+
+		//GA Calls
+		var start_date = '';
+		var end_date = '';
+		var data = {
+			'action': 'ga_calls',
+			'post_id': post_id,
+			'start_date': start_date,
+			'end_date': end_date
+		};
+		$.ajax({
+			method: "POST",
+			url: ajaxurl,
+			data: data,
+			dataType: 'json'
+		})
+			.done(function( response ) {
+				if ( response.error !== undefined && response.error == false ) 
+				{
+					$("#users_count").empty().html( numberWithCommas( response.data.desktop.users_count ) );
+					$("#sessions_count").empty().html( numberWithCommas( response.data.desktop.sessions_count ) );
+					$("#avg_sessions_duration").empty().html( secondsToMinSeconds( response.data.desktop.avg_sessions_duration ) );
+					$("#pageviews_count").empty().html( numberWithCommas( response.data.desktop.pageviews_count ) );
+					$("#pageviews_per_session").empty().html( response.data.desktop.pageviews_per_session );
+				};
+			});
+
+		/* Special date widget */
+	 
+		var to = new Date();
+		var from = new Date();
+				from.setDate( from.getDate() - 90);
+		
+		$('#datepicker-calendar').DatePicker({
+			inline: true,
+			date: [from, to],
+			calendars: 3,
+			mode: 'range',
+			starts: 1,
+			locale: {
+				daysMin: ["D", "L", "M", "M", "J", "V", "S"],
+				months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "June¡io", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Deciembre"],
+				monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+			},
+			current: new Date(to.getFullYear(), to.getMonth() - 1, 1),
+			onChange: function(dates,el) {
+				// update the range display
+				$('#date-range-field span').text(dates[0].getDate()+' '+dates[0].getMonthName(true)+', '+dates[0].getFullYear()+' - '+
+																		dates[1].getDate()+' '+dates[1].getMonthName(true)+', '+dates[1].getFullYear());
+			}
+		});
+		
+		// initialize the special date dropdown field
+		$('#date-range-field span').text(from.getDate()+' '+from.getMonthName(true)+', '+from.getFullYear()+' - '+
+																		to.getDate()+' '+to.getMonthName(true)+', '+to.getFullYear());
+		
+		// bind a click handler to the date display field, which when clicked
+		// toggles the date picker calendar, flips the up/down indicator arrow,
+		// and keeps the borders looking pretty
+		$('#date-range-field').bind('click', function(){
+			$('#datepicker-calendar').toggle();
+			if($('#date-range-field a').text().charCodeAt(0) == 9660) {
+				// switch to up-arrow
+				$('#date-range-field a').html('&#9650;');
+				$('#date-range-field').css({borderBottomLeftRadius:0, borderBottomRightRadius:0});
+				$('#date-range-field a').css({borderBottomRightRadius:0});
+			} else {
+				// switch to down-arrow
+				$('#date-range-field a').html('&#9660;');
+				$('#date-range-field').css({borderBottomLeftRadius:5, borderBottomRightRadius:5});
+				$('#date-range-field a').css({borderBottomRightRadius:5});
+			}
+			return false;
+		});
+		
+		// global click handler to hide the widget calendar when it's open, and
+		// some other part of the document is clicked.  Note that this works best
+		// defined out here rather than built in to the datepicker core because this
+		// particular example is actually an 'inline' datepicker which is displayed
+		// by an external event, unlike a non-inline datepicker which is automatically
+		// displayed/hidden by clicks within/without the datepicker element and datepicker respectively
+		$('html').click(function() {
+			if($('#datepicker-calendar').is(":visible")) {
+				$('#datepicker-calendar').hide();
+				$('#date-range-field a').html('&#9660;');
+				$('#date-range-field').css({borderBottomLeftRadius:5, borderBottomRightRadius:5});
+				$('#date-range-field a').css({borderBottomRightRadius:5});
+			}
+		});
+
+		$('#cancel_ga').click(function(event){
+			if($('#datepicker-calendar').is(":visible")) {
+				$('#datepicker-calendar').hide();
+				$('#date-range-field a').html('&#9660;');
+				$('#date-range-field').css({borderBottomLeftRadius:5, borderBottomRightRadius:5});
+				$('#date-range-field a').css({borderBottomRightRadius:5});
+			}
+
+			event.preventDefault();
+		});
+
+		$('#apply_ga').click(function(event){
+			var dates_filter = $('#datepicker-calendar').DatePickerGetDate();
+			var init_date = new Date( dates_filter[0][0] );
+			var end_date = new Date( dates_filter[0][1] );
+
+			var data = {
+				'action': 'ga_calls',
+				'post_id': post_id,
+				'start_date': init_date.yyyymmdd(),
+				'end_date': end_date.yyyymmdd()
+			};
+			$.ajax({
+				method: "POST",
+				url: ajaxurl,
+				data: data,
+				dataType: 'json'
+			})
+				.done(function( response ) {
+					if ( response.error !== undefined && response.error == false ) 
+					{
+						$("#users_count").empty().html( numberWithCommas( response.data.desktop.users_count ) );
+						$("#sessions_count").empty().html( numberWithCommas( response.data.desktop.sessions_count ) );
+						$("#avg_sessions_duration").empty().html( secondsToMinSeconds( response.data.desktop.avg_sessions_duration ) );
+						$("#pageviews_count").empty().html( numberWithCommas( response.data.desktop.pageviews_count ) );
+						$("#pageviews_per_session").empty().html( response.data.desktop.pageviews_per_session );
+					};
+				});
+
+			event.preventDefault();
+		});
+		
+		// stop the click propagation when clicking on the calendar element
+		// so that we don't close it
+		$('#datepicker-calendar').click(function(event){
+			event.stopPropagation();
+		});
+	};
 });
 
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+function numberWithCommas(x)
+{
+		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
-function secondsToMinSeconds ( time ) {
+function secondsToMinSeconds ( time )
+{
 	var minutes = Math.floor(time / 60);
 	var seconds = time - minutes * 60;
 
 	return minutes + ' :' + seconds;
 }
+
+Date.prototype.yyyymmdd = function() {
+	var yyyy = this.getFullYear().toString();
+	var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
+	var dd  = this.getDate().toString();
+
+	return yyyy + "-" + (mm[1]?mm:"0"+mm[0]) + "-" + (dd[1]?dd:"0"+dd[0]); // padding
+};
--- mediakit-ga/mediakit-ga.php
+++ mediakit-ga/mediakit-ga.php
@@ -22,29 +22,74 @@ require_once 'Google/Client.php';
 require_once 'Google/Service/Analytics.php';
 
 add_action('wp_head', 'head_setup_mt');
-function head_setup_mt() {
+function head_setup_mt() 
+{
+	if ( is_single() ) 
+	{
+	
+		global $post;
+		wp_enqueue_script( 'ajax-script', plugins_url( '/js/mediakit.js', __FILE__ ), array('jquery') );
+
+		$js_datepicker = plugins_url( 'datepicker/js/datepicker.js', __FILE__ );
+		$css_base_datepicker = plugins_url( 'datepicker/css/base.css', __FILE__ );
+		$css_clean_datepicker = plugins_url( 'datepicker/css/clean.css', __FILE__ );
+		$css_custom_datepicker = plugins_url( 'datepicker/css/custom.css', __FILE__ );
+?>
+
+		<script type="text/javascript">
+			if (!window.jQuery) {
+				var jq = document.createElement('script'); jq.type = 'text/javascript';
+				jq.src = '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js';
+				document.getElementsByTagName('head')[0].appendChild(jq);
+			}
+			var is_single = <?php echo is_single(); ?>;
+			var ajaxurl = '<?php echo admin_url("admin-ajax.php"); ?>';
+			var post_id = <?php echo $post->ID; ?>
+		</script>
+		<script type="text/javascript" src="<?php echo $js_datepicker; ?>"></script>
+		<link rel="stylesheet" type="text/css" href="<?php echo $css_base_datepicker; ?>" />
+		<link rel="stylesheet" type="text/css" href="<?php echo $css_clean_datepicker; ?>" />
+		<link rel="stylesheet" type="text/css" href="<?php echo $css_custom_datepicker; ?>" />
+
+<?php
+	}
+}
+
+#Google Analyctics calls
+add_action( 'wp_ajax_ga_calls', 'ga_calls_callback' );
+add_action( 'wp_ajax_nopriv_ga_calls', 'ga_calls_callback' );
+function ga_calls_callback() 
+{
 	global $post;
-
+	
+	#Sets
+	$path_plugin = plugin_dir_path( __FILE__ );
 	$ga_account = '';
-	$facebook_page = '';
-	$twitter_profile = '';
-	$instagram_id = '';
-	$likes_fb = 0;
-	$twitter_count = 0;
-	$instagram_count = 0;
-
+	$has_error = true;
 	#GA Stats
 	$sessions_count = 0;
 	$users_count = 0;
 	$avg_sessions_duration = 0;
 	$pageviews_count = 0;
 	$pageviews_per_session = 0;
+	#GA Stats Mobile
+	$sessions_count_mobile = 0;
+	$users_count_mobile = 0;
+	$avg_sessions_duration_mobile = 0;
+	$pageviews_count_mobile = 0;
+	$pageviews_per_session_mobile = 0;
+	
 
-	if ( function_exists('get_field') ) {
-		$path_plugin = plugin_dir_path( __FILE__ );
-		#Google Analytics
-		$ga_account = get_field('ga_account', $post->ID);
-		if ( $ga_account  ) {
+	if ( function_exists('get_field') ) 
+	{
+		
+		$post_id = $_POST['post_id'];
+		$start_date = $_POST['start_date'];
+		$end_date = $_POST['end_date'];
+		$ga_account = get_field('ga_account', $post_id);
+
+		if ( $ga_account  ) 
+		{
 			// Declaramos nuestra configuración 
 			$googleApi = array(
 				'id' => '999359394470-cg7pe74pt9uqauoupd6cjjfdf5q5nsv3.apps.googleusercontent.com', // Id que nos ha dado la APIs Console
@@ -68,79 +113,195 @@ function head_setup_mt() {
 
 			$service = new Google_Service_Analytics($client);
 			
-			$results = $service->data_ga->get(
-				$googleApi['gaAccount'], 
-				'90daysAgo', 
-				'yesterday', 
+			// Set dates params
+			// Start date
+			if ( empty( trim( $start_date ) ) ) 
+			{
+				$start_date = '90daysAgo';
+			}
+			// End date
+			if ( empty( trim( $end_date ) ) ) 
+			{
+				$end_date = 'yesterday';
+			}
+
+			$resultsDesktop = $service->data_ga->get(
+				$googleApi['gaAccount'],
+				$start_date,
+				$end_date,
 				'ga:users,ga:sessions,ga:avgSessionDuration,ga:pageviews,ga:pageviewsPerSession',
 				array(
 					// 'filter' => 'ga:medium==organic',
-					// 'dimensions' => 'ga:deviceCategory'
+					'dimensions' => 'ga:deviceCategory'
 				)
 			);
-			if ( $results->totalsForAllResults ) {
-				$users_count = ceil( $results->totalsForAllResults['ga:users'] );
-				$sessions_count = ceil( $results->totalsForAllResults['ga:sessions'] );
-				$avg_sessions_duration = ceil( $results->totalsForAllResults['ga:avgSessionDuration'] );
-				$pageviews_count = ceil( $results->totalsForAllResults['ga:pageviews'] );
-				$pageviews_per_session = $results->totalsForAllResults['ga:pageviewsPerSession'];
+
+			if ( $resultsDesktop->totalsForAllResults ) 
+			{
+				//Totales
+				$users_count = ceil( $resultsDesktop->totalsForAllResults['ga:users'] );
+				$sessions_count = ceil( $resultsDesktop->totalsForAllResults['ga:sessions'] );
+				$avg_sessions_duration = ceil( $resultsDesktop->totalsForAllResults['ga:avgSessionDuration'] );
+				$pageviews_count = ceil( $resultsDesktop->totalsForAllResults['ga:pageviews'] );
+				$pageviews_per_session = $resultsDesktop->totalsForAllResults['ga:pageviewsPerSession'];
+
+				//Mobile
+				if ( count( $resultsDesktop->rows > 0 ) ) 
+				{
+					foreach ( $resultsDesktop->rows as $key => $row ) {
+						if ( $row[0] == 'mobile' || $row[0] == 'tablet' ) {
+							$users_count_mobile += $row[1];
+							$sessions_count_mobile += $row[2];
+							$avg_sessions_duration_mobile += $row[3];
+							$pageviews_count_mobile += $row[4];
+							$pageviews_per_session_mobile += $row[5];
+						}
+					}
+				}
+				$avg_sessions_duration_mobile = $avg_sessions_duration_mobile / 2;
+				// $avg_sessions_duration_mobile = number_format($avg_sessions_duration_mobile , 2, '.', '');
+
+				$pageviews_per_session_mobile = $pageviews_per_session_mobile / 2;
+				// $pageviews_per_session_mobile = number_format($pageviews_per_session_mobile , 2, '.', '');
+
+				$response =  array(
+							"desktop" => array(
+									"users_count" => $users_count,
+									"sessions_count" => $sessions_count,
+									"avg_sessions_duration" => $avg_sessions_duration,
+									"pageviews_count" => $pageviews_count,
+									"pageviews_per_session" => $pageviews_per_session,
+								),
+							"mobile" => array(
+									"users_count" => $users_count_mobile,
+									"sessions_count" => $sessions_count_mobile,
+									"avg_sessions_duration" => $avg_sessions_duration_mobile,
+									"pageviews_count" => $pageviews_count_mobile,
+									"pageviews_per_session" => $pageviews_per_session_mobile,
+								)
+						);
+
+				$has_error = false;
 			}
 		}
+	}
 
+	echo json_encode( array( "error" => $has_error, "data" => $response ) );
+	wp_die();
+}
+
+#Facebook calls
+add_action( 'wp_ajax_fb_calls', 'fb_calls_callback' );
+add_action( 'wp_ajax_nopriv_fb_calls', 'fb_calls_callback' );
+function fb_calls_callback() 
+{
+	global $post;
+
+	$post_id = $_POST['post_id'];
+	$facebook_page = '';
+	$likes_fb = 0;
+	$has_error = true;
+
+	if ( function_exists('get_field') ) 
+	{
 		#Facebook likes
-		$facebook_page = get_field('facebook_page', $post->ID);
-		if ( $facebook_page  ) {
+		$facebook_page = get_field('facebook_page', $post_id);
+		if ( $facebook_page  ) 
+		{
 			$likes_fb = fbLikeCount( trim( $facebook_page ) );
+			$response =  array(
+							"likes_fb" => $likes_fb
+						);
+
+			$has_error = false;
 		}
+	}
+
+	echo json_encode( array( "error" => $has_error, "data" => $response ) );
+	wp_die();
+}
+
+#Twitter calls
+add_action( 'wp_ajax_tw_calls', 'tw_calls_callback' );
+add_action( 'wp_ajax_nopriv_tw_calls', 'tw_calls_callback' );
+function tw_calls_callback() 
+{
+	global $post;
+
+	$post_id = $_POST['post_id'];
+	$twitter_profile = '';
+	$twitter_count = 0;
+	$has_error = true;
+
+	if ( function_exists('get_field') ) 
+	{
 
 		#Twitter followers
-		$twitter_profile = get_field('twitter_profile', $post->ID);
-		if ( $twitter_profile  ) {
+		$twitter_profile = get_field('twitter_profile', $post_id);
+		if ( $twitter_profile  ) 
+		{
 			$followers_count_response = file_get_contents( 'https://cdn.syndication.twimg.com/widgets/followbutton/info.json?screen_names=' . trim( $twitter_profile ) );
-			if ( $followers_count_response ) {
+			if ( $followers_count_response ) 
+			{
 				$followers_count_response = json_decode( $followers_count_response );
 				$twitter_count = $followers_count_response[0]->followers_count;
-			}
-		}
 
-		#Instagram followers
-		$instagram_id = get_field('instagram_id', $post->ID);
-		if ( $instagram_id  ) {
-			$followers_count_response = file_get_contents( 'https://api.instagram.com/v1/users/' . trim( $instagram_id ) . '/?access_token=53042481.ab103e5.0c6f8f50471a4e1f97595f8db529a47a' );
-			if ( $followers_count_response ) {
-				$followers_count_response = json_decode( $followers_count_response );
-				$instagram_count = $followers_count_response->data->counts->followed_by;
+				$response =  array(
+								"twitter_count" => $twitter_count
+							);
+
+				$has_error = false;
 			}
 		}
-	} else {
 
 	}
 
-	$js_path = plugins_url( 'js/mediakit.js', __FILE__ );
-?>
-
-	<script type="text/javascript">
-		if (!window.jQuery) {
-			var jq = document.createElement('script'); jq.type = 'text/javascript';
-			jq.src = '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js';
-			document.getElementsByTagName('head')[0].appendChild(jq);
-		}
-		var fb_likes = '<?php echo $likes_fb; ?>';
-		var twitter_followers = '<?php echo $twitter_count; ?>';
-		var instagram_followers = '<?php echo $instagram_count; ?>';
-		//GA
-		var sessions_count = '<?php echo $sessions_count; ?>'
-		var users_count = '<?php echo $users_count; ?>'
-		var avg_sessions_duration = '<?php echo $avg_sessions_duration; ?>'
-		var pageviews_count = '<?php echo $pageviews_count; ?>'
-		var pageviews_per_session = '<?php echo $pageviews_per_session; ?>'
-	</script>
-	<script type="text/javascript" src="<?php echo $js_path ?>"></script>
-<?php
+	echo json_encode( array( "error" => $has_error, "data" => $response ) );
+	wp_die();
 }
 
+#Instagram calls
+add_action( 'wp_ajax_inst_calls', 'inst_calls_callback' );
+add_action( 'wp_ajax_nopriv_inst_calls', 'inst_calls_callback' );
+function inst_calls_callback() 
+{
+	global $post;
+
+	$post_id = $_POST['post_id'];
+	$instagram_id = '';
+	$instagram_count = 0;
+	$has_error = true;
+
+	if ( function_exists('get_field') ) 
+	{
+
+		#Instagram followers
+		$instagram_id = get_field('instagram_id', $post_id);
+		if ( $instagram_id  ) 
+		{
+			$followers_count_response = file_get_contents( 'https://api.instagram.com/v1/users/' . trim( $instagram_id ) . '/?access_token=53042481.ab103e5.0c6f8f50471a4e1f97595f8db529a47a' );
+			if ( $followers_count_response ) 
+			{
+				$followers_count_response = json_decode( $followers_count_response );
+				$instagram_count = $followers_count_response->data->counts->followed_by;
+
+				$response =  array(
+								"instagram_count" => $instagram_count
+							);
+
+				$has_error = false;
+			}
+		}
+	}
+
+	echo json_encode( array( "error" => $has_error, "data" => $response ) );
+	wp_die();
+}
+
+
 //Get Facebook Likes Count of a page
-function fbLikeCount($id){
+function fbLikeCount($id)
+{
 	//Construct a Facebook URL
 	$json_url ='https://graph.facebook.com/'.$id.'';
 	$json = file_get_contents($json_url);
